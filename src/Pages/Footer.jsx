3cdafdd8319b58971960b2d6118e342eaeb7a491
shavuparmar <parmarshavu009@gmail.com>
2025-10-16 12:24:10 +0530

    footer change

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="fixed bottom-0 left-0 right-0 z-50">
+    <footer className="fixed bottom-12 left-0 right-0 z-50">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="my-3 flex items-center justify-center">
           <div className="glass-card w-full sm:w-auto rounded-full py-2 px-4 text-center text-xs sm:text-sm text-gray-100">
